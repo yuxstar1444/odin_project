c1d3cf54c2fd910c2eee6cfbfad6d113e33cbace
yuxstar1444@protonmail.com <yuxstar1444@protonmail.com>
2022-11-04 19:46:21 -0400

    begun js

---
+++ etch-a-sketch/script.js
@@ -0,0 +1,15 @@
+const DEFAULT_COLOR = "#333333"
+const DEFAULT_MODE = 'color'
+const DEFAULT_SIZE = 16
+
+let currentColor = DEFAULT_COLOR
+let currentMode = DEFAULT_MODE
+let currentSize = DEFAULT_SIZE
+
+function setCurrentColor(newColor){
+
+}
+
+function setCurrentMode(newMode){
+
+}
