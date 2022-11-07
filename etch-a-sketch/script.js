78e1fe456c27849ad474a932e5ff08ffeffa0047
yuxstar1444@protonmail.com <yuxstar1444@protonmail.com>
2022-11-07 14:23:20 -0500

    updated js

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -58,14 +58,31 @@ function reloadGrid(){
 }
 
 function clearGrid(){
-
+    grid.innerHTML = ''
 }
 
 function setupGrid(){
+    grid.style.gridTemplateColumns = 'repeat(${size}, 1fr)'
+    grid.style.gridTemplateRows = 'repeat(${size}, 1fr)'
 
+    for(let i = 0; i < size * size; i++){
+        const gridElement = document.createElement('div')
+        gridElement.classList.add('grid-element')
+        gridElement.addEventListener('mouseover', changeColor)
+        gridElement.addEventListener('mousedown', changeColor)
+        grid.appendChild(gridElement)
+
+    }
 }
 
-function changeColor(){
+function changeColor(e){
+    if (e.type == 'mouseover' && !mouseDown)
+        return
+    if (currentMode == 'rainbow'){
+        const randomR = Math.floor(Math.random()*256)
+        const randomG = Math.floor(Math.random()*256)
+        const randomB = Math.floor(Math.random()*256)
+    }
 
 }
 
